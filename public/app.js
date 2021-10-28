edd2313b168456d098c66bc4e3f05a7118e19a2e
diegoarcos7 <arcos.5c@gmail.com>
2021-10-28 18:04:54 -0500

    Dando estilos al Todo

--- public/app.js
+++ public/app.js
@@ -18,7 +18,7 @@ function addTodo(event){
     todoDiv.classList.add("todo");
     //Create LI
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     //CHECK MARK BUTTON
@@ -29,8 +29,10 @@ function addTodo(event){
     //CHECK trash BUTTON
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>';
-    trashButton.classList.add("complete-btn");
+    trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
     //APPEND TO LIST
-    todoList.appendChild(todoDiv);
+    todoList.appendChild(todoDiv); 
+    //Clear TodoInput Value
+    
 }
